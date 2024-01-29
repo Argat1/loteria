75c7a8ddd0dd2451826415765cdc57bd5aba4b36
Łukasz Jabłoński <lukasz.jablonski44@gmail.com>
2024-01-29 09:47:14 +0100

    Added_MainPage

--- loteria/loteria/MainPage.xaml.cs
+++ loteria/loteria/MainPage.xaml.cs
@@ -5,15 +5,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace loteria
 {
     public partial class MainPage : ContentPage
     {
-        
+        [XamlCompilation(XamlCompilationOptions.Compile)]
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void gracz_nav_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new StonaGracz());
+        }
+
+        private void admin_Nnav_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new StronaAdmin());
+        }
+
+        private void wyniki_nav_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new StronaWynik());
         }
     }
 }
