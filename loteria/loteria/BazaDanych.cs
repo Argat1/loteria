5cadcaacac02defa8c0c7f01904ee21877b98d6f
Łukasz Jabłoński <lukasz.jablonski44@gmail.com>
2024-01-29 09:50:41 +0100

    Added_classes

---
+++ loteria/loteria/BazaDanych.cs
@@ -0,0 +1,27 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace loteria
+{
+    public class BazaDanych
+    {
+        private readonly SQLiteConnection polaczenie;
+        public BazaDanych(string sciezka)
+        {
+            polaczenie = new SQLiteConnection(sciezka);
+            polaczenie.CreateTable<Gracz>();
+            polaczenie.CreateTable<Wynik>();
+        }
+        public int Zapisz<T>(T obiekt)
+        {
+            return polaczenie.Insert(obiekt);
+        }
+        public List<T> Wypisz<T>() where T : new()
+        {
+            return polaczenie.Table<T>().ToList();
+        }
+    }
+}
