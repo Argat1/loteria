a56f382193eadaf1e983c590607c4bf5279087cd
Łukasz Jabłoński <lukasz.jablonski44@gmail.com>
2024-01-29 10:02:14 +0100

    Added_and_fixed_code_in_classes

--- loteria/loteria/Gracz.cs
+++ loteria/loteria/Gracz.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,28 @@ namespace loteria
 {
     internal class Gracz
     {
+        [AutoIncrement, PrimaryKey]
+        public int Id { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Email { get; set; }
+        public string Kod { get; set; }
+        public Gracz(int id, string imie, string nazwisko, string email, string kod)
+        {
+            Id = id;
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Email = email;
+            Kod = kod;
+        }
+        public Gracz(string imie, string nazwisko, string email, string kod)
+        {
+
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Email = email;
+            Kod = kod;
+        }
+        public Gracz() {}
     }
 }
--- loteria/loteria/Wynik.cs
+++ loteria/loteria/Wynik.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,27 @@ namespace loteria
 {
     internal class Wynik
     {
+       
+            [AutoIncrement, PrimaryKey]
+            public int Id { get; set; }
+            public DateTime DataLosowania { get; set; }
+            public string WygraneLiczby { get; set; }
+            public int IloscWygranych { get; set; }
+
+            public Wynik() { }
+            public Wynik(int id, DateTime dataLosowania, string wygraneLiczby, int iloscWygranych)
+            {
+                Id = id;
+                DataLosowania = dataLosowania;
+                WygraneLiczby = wygraneLiczby;
+                IloscWygranych = iloscWygranych;
+            }
+            public Wynik(DateTime dataLosowania, string wygraneLiczby, int iloscWygranych)
+            {
+                DataLosowania = dataLosowania;
+                WygraneLiczby = wygraneLiczby;
+                IloscWygranych = iloscWygranych;
+            }
+        
     }
 }
